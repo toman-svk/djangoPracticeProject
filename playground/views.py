0844456c288e9a73afe42d4c72a6f9f416b6eebd
tomansvk <tomanka.filip@gmail.com>
2025-01-15 21:51:51 +0100

    adding debug django app

--- playground/views.py
+++ playground/views.py
@@ -5,5 +5,12 @@ from django.http import HttpResponse
 # request -> response
 # also called request handler
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+
 def say_hello(request):
+    x = calculate()
     return render(request, 'hello.html', {'name':'Filip'})
