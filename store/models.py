e5ffd965502a4ed34e015e5f83f6ef72bf493efd
tomansvk <tomanka.filip@gmail.com>
2025-01-19 18:11:43 +0100

    creating data models

--- store/models.py
+++ store/models.py
@@ -10,8 +10,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255) #varchar 255
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2) #always use DecimalFields for monetary values
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2) #always use DecimalFields for monetary values
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -34,6 +35,11 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices = MEMBERSHIP_CHOICES, default = MEMBERSHIP_BRONZE)
 
+    class Meta:
+        db_table = 'store_customers'
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'])
+        ]
 
 class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
