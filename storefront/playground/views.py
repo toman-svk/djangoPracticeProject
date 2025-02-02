3192554aa9d5655acdf17fb25bd6e0da27804d43
tomansvk <tomanka.filip@gmail.com>
2025-02-02 17:44:38 +0100

    Filtering Objects

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
-
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
 def say_hello(request):
-    x = calculate()
+    #None
+    queryset = Product.objects.filter(unit_price = 20)
+
     return render(request, 'hello.html', {'name': 'Mosh'})
