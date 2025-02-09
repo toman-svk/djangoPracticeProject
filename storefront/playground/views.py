d66e8771f95b51d0a2c8ce4325d7012393c0ce0c
tomansvk <tomanka.filip@gmail.com>
2025-02-09 20:15:15 +0100

    Edit return query

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -6,6 +6,6 @@ from store.models import Product
 
 def say_hello(request):
     #None
-    queryset = Product.objects.filter(unit_price = 20)
+    queryset = Product.objects.filter(title__icontains = 'coffee')
 
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, 'hello.html', {'name': 'Filip', 'products': list(queryset)})
